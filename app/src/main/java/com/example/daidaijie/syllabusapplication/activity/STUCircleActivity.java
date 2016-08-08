b9e673554d70eb1f7c951768b03c9d323c328828
daidaijie <317095153@qq.com>
2016-08-08 21:57:21 +0800

    修改了下菜单的小细节

---
+++ app/src/main/java/com/example/daidaijie/syllabusapplication/activity/STUCircleActivity.java
@@ -0,0 +1,19 @@
+package com.example.daidaijie.syllabusapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.daidaijie.syllabusapplication.R;
+
+public class STUCircleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_stucircle;
+    }
+}
